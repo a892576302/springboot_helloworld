ecad41f2396979c76bbb267d2e0b7fa33709a608
非洲小黑 <892576302@qq.com>
2018-02-26 11:48:51 +0800

    freeMaker

--- src/main/java/com/xiaohei/controller/FreeMakerController.java
+++ src/main/java/com/xiaohei/controller/FreeMakerController.java
@@ -8,11 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Created by 非洲小黑 on 2018/2/26.
  */
 @Controller
+@RequestMapping(value = "freeMaker")
 public class FreeMakerController {
 
-    @RequestMapping(value = "hello3")
-    public String hello3(Model model){
+    @RequestMapping()
+    public String freeMaker(Model model){
         model.addAttribute("msg","freeMaker的视图");
-        return "hello3";
+        return "freeMaker";
     }
 }
