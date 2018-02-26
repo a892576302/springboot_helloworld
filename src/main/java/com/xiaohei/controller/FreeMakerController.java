303e293ba4c24bae3b9b7e900d39b7334a3dadc2
非洲小黑 <892576302@qq.com>
2018-02-26 11:55:27 +0800

    添加FreeMake方法

--- src/main/java/com/xiaohei/controller/FreeMakerController.java
+++ src/main/java/com/xiaohei/controller/FreeMakerController.java
@@ -3,6 +3,7 @@ package com.xiaohei.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by 非洲小黑 on 2018/2/26.
@@ -15,5 +16,11 @@ public class FreeMakerController {
     public String freeMaker(Model model){
         model.addAttribute("msg","freeMaker的视图");
         return "freeMaker";
+    }
+
+    @RequestMapping(value="hello",method = RequestMethod.GET)
+    public String hello(Model model){
+        model.addAttribute("msg","freeMaker的hello的视图");
+        return "freeMaker/hello";
     }
 }
