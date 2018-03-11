5ba8f053a501ed8f6c3b775c087486ae5325c943
非洲小黑 <892576302@qq.com>
2018-03-11 14:17:23 +0800

    修改

--- src/main/java/com/xiaohei/App.java
+++ src/main/java/com/xiaohei/App.java
@@ -2,13 +2,24 @@ package com.xiaohei;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.activation.DataSource;
 
 /**
  * Created by 非洲小黑 on 2018/1/31.
  */
 @SpringBootApplication
 public class App {
-    public static void main(String[] args) {
-        SpringApplication.run(App.class,args);
+    /**
+     * 代码的方式实现datasource
+     */
+    @Bean
+    public DataSource getDataSource(){
+        return null;
     }
+
+    /*public static void main(String[] args) {
+        SpringApplication.run(App.class,args);
+    }*/
 }
