ea4df447c2c1f22b309b543e592296e7c453444d
a892576302 <892576302@qq.com>
2019-08-22 19:14:13 +0800

    Update HelloWorldController.java

--- src/main/java/com/xiaohei/controller/HelloWorldController.java
+++ src/main/java/com/xiaohei/controller/HelloWorldController.java
@@ -22,6 +22,6 @@ public class HelloWorldController {
     public String hello(Model model) {
         System.out.println(randomint);
         System.out.println(args.getNonOptionArgs().toString());
-        return "hello world";
+        return "hello world 哈哈哈";
     }
 }
